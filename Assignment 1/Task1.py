890a020b5e3884583fb0b7d27e2bb5993cd4f0ab
alokahuja82 <66566419+alokahuja82@users.noreply.github.com>
2025-07-17 18:11:44 +0530

    Add files via upload

---
+++ Assignment 1/Task1.py
@@ -0,0 +1,20 @@
+#Task1
+a =int(input("Enter the first number: "))
+b =int(input("Enter the Second number: "))
+
+#addition
+add = a+b
+print('Addition:',add)
+
+#subtraction
+sub = a-b
+print('Subtraction:',sub)
+
+#multiply
+mul = a*b
+print('Multiply:',mul)
+
+#division
+div = a/b
+print('Division:',div)
+
---
+++ Assignment 1/Task2.py
@@ -0,0 +1,7 @@
+#Task2
+first = input("Enter your First name: ")
+last = input("Enter your Last name: ")
+Name = first + " " + last
+
+
+print("Hello " + Name + "! Welcome to the python program.")
