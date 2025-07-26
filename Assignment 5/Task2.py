27326562012576e6494548d7ef202cb5eb61a037
alokahuja82 <66566419+alokahuja82@users.noreply.github.com>
2025-07-26 17:05:26 +0530

    Create Task2.py

---
+++ Assignment 5/Task2.py
@@ -0,0 +1,6 @@
+li = [i for i in range(1,11)]
+print("Orignal list:",li)
+ff_li = li[0:5]
+print("Extracted first five elements:",ff_li)
+rev_ff_li = ff_li[::-1]
+print("Reversed extracted elements:",rev_ff_li)
